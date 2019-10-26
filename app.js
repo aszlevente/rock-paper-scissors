d61ca7e0f230a3a548accffd3e1f5040f07b0e96
Ambrus-Szabó Levente <levilibikoka@gmail.com>
2019-10-26 18:01:09 +0200

    ready javascipt

--- app.js
+++ app.js
@@ -40,25 +40,34 @@ function convertWord (letter) {
 }
     
 function win (userChoice, computerChoice) {
+  var smallUser = 'user'.fontsize(3).sub();
+  var smallComp = 'comp'.fontsize(3).sub();
+  var userChoice_div = document.getElementById(userChoice);
   userScore++
   userScore_span.innerHTML = userScore;
   compScore_span.innerHTML = compScore;
-  var smallUser = 'user'.fontsize(3).sub();
-  var smallComp = 'comp'.fontsize(3).sub();
   result_p.innerHTML = `${convertWord(userChoice)}${smallUser} beats ${convertWord(computerChoice)}${smallComp}. You win!`;
+  userChoice_div.classList.add('green');
+  setTimeout(function() { userChoice_div.classList.remove('green') }, 300);
 }
 
 function lose (userChoice, computerChoice) {
+  var smallUser = 'user'.fontsize(3).sub();
+  var smallComp = 'comp'.fontsize(3).sub();
+  var userChoice_div = document.getElementById(userChoice);
   compScore++
   userScore_span.innerHTML = userScore;
   compScore_span.innerHTML = compScore;
-  var smallUser = 'user'.fontsize(3).sub();
-  var smallComp = 'comp'.fontsize(3).sub();
   result_p.innerHTML = `${convertWord(userChoice)}${smallUser} loses to ${convertWord(computerChoice)}${smallComp}. You lost!`;
+  userChoice_div.classList.add('red');
+  setTimeout(function() { userChoice_div.classList.remove('red') }, 300);
 }
 
 function draw (userChoice, computerChoice) {
+  var userChoice_div = document.getElementById(userChoice);
   result_p.innerHTML = `${convertWord(userChoice)} equals ${convertWord(computerChoice)}. Its a draw!`;
+  userChoice_div.classList.add('gray');
+  setTimeout(function() { userChoice_div.classList.remove('gray') }, 300);
 }
 
 function getComputerChoice () {
